12128a9dd04d70e67a1b7453467d16cd3d9e021e
sujaysundar <61531742+sujaysundar@users.noreply.github.com>
2021-03-01 00:19:27 -0500

    Delete FBLAState2021 directory

--- FBLAState2021/data/DDL_DELETE.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM answer;
-DELETE FROM student_score;
-DELETE FROM student;
-DELETE FROM question;
-
-COMMIT;
--- FBLAState2021/tests/testenvironment.py
+++
@@ -1,3 +0,0 @@
-#Test to check if the environment is set to execute a Python program. If the below statement prints on the console, it indicates the environment is set correctly to run a Python program. 
-
-print('Python environment is setup correctly.')  
